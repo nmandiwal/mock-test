a1056881e241056d2e2481721896a6de034eb836
nmandiwal <nikimca@gmail>
2024-09-02 11:11:39 +0100

    draft code

--- src/main/java/com/mandiwal/test/data/Chapter.java
+++ src/main/java/com/mandiwal/test/data/Chapter.java
@@ -4,8 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum Chapter {
-    CBSE_6_SCIENCE_1("CBSE Class 6 Chapter 1", "class_6_science_chapter_1.json", "Components of Food"),
-    CBSE_6_SCIENCE_2("CBSE Class 6 Chapter 2", "class_6_science_chapter_2.json", "Sorting Materials into Groups");
+    CBSE_6_SCIENCE_1("CBSE Class 6 Chapter 1", "cbse/6/1.json", "Components of Food"),
+    CBSE_6_SCIENCE_2("CBSE Class 6 Chapter 2", "cbse/6/2.json", "Sorting Materials into Groups"),
+    CBSE_6_SCIENCE_3("CBSE Class 6 Chapter 3", "cbse/6/3.json", "Separation of Substances"),
+    CBSE_6_SCIENCE_4("CBSE Class 6 Chapter 4", "cbse/6/4.json", "Getting to Know Plants"),
+    CBSE_6_SCIENCE_5("CBSE Class 6 Chapter 5", "cbse/6/5.json", "Body Movements");
 
     private final String displayText;
     private final String fileName;
